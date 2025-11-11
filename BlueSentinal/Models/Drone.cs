ea374626f0f361da31bf1c1d72ff848da9d40ef6
matheus2306 <math2306.r@gmail.com>
2025-11-11 13:57:15 -0300

    Refatora modelos e adiciona validações
    Refatora o modelo `Drone`, removendo propriedades antigas e
    adicionando novas relacionadas a chaves estrangeiras:
    `DroneFabriId`, `DroneFabri`, `UsuarioId` e `Usuario`.
    
    Cria o modelo `DroneFabri` com propriedades validadas, como
    `Modelo`, `Mac`, `Status`, `Localizacao` e `TempoAtividade`.
    
    Atualiza o modelo `Usuario`, substituindo `Nome` e `Nascimento`
    por versões com validações adicionais, incluindo `[Required]`,
    limites de comprimento e mensagens de erro personalizadas.
    
    As alterações melhoram a estrutura e garantem validações
    consistentes entre os modelos.

--- BlueSentinal/Models/Drone.cs
+++ BlueSentinal/Models/Drone.cs
@@ -5,10 +5,13 @@ namespace BlueSentinal.Models
     public class Drone
     {
         public Guid DroneId { get; set; }
-        [Required(ErrorMessage = "Modelo de drone é obrigatório")]
 
-        public string? Modelo { get; set; }
-        public string? Mac { get; set; }
-        public bool? Status { get; set; }
+        //chave estrangeira
+        public Guid? DroneFabriId { get; set; }
+        public DroneFabri? DroneFabri { get; set; }
+        public Guid? UsuarioId { get; set; }
+        public Usuario? Usuario { get; set; }
+
+
     }
 }
---
+++ BlueSentinal/Models/DroneFabri.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlueSentinal.Models
+{
+    public class DroneFabri
+    {
+        public Guid DroneFabriId { get; set; }
+        [Required(ErrorMessage = "Modelo de drone é obrigatório")]
+        public string? Modelo { get; set; }
+        [Required(ErrorMessage = "MAC é obrigatório")]
+        public string? Mac { get; set; }
+        [Required(ErrorMessage = "Status é obrigatório")]
+        public bool? Status { get; set; }
+        [Required(ErrorMessage = "Localização é obrigatória")]
+        public string Localizacao { get; set; }
+        public int TempoAtividade { get; set; }
+    }
+}
